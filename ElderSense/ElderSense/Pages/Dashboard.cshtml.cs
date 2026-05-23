5b2a000e27a4d633790a85d5b4bd0a2b78c5c457
T0masS7 <tomassegurado07@icloud.com>
2026-05-23 21:26:33 +0100

    corrigir fluxo de login em dois passos com campo hidden para email

--- ElderSense/ElderSense/Pages/Dashboard.cshtml.cs
+++ ElderSense/ElderSense/Pages/Dashboard.cshtml.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ElderSense.Pages
 {
+    // Apenas utilizadores autenticados podem aceder ao dashboard
+    [Authorize]
     public class DashboardModel : PageModel
     {
         public void OnGet()
