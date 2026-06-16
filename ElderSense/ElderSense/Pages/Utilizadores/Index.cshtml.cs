6952c180c093fa02b4f05c7577de817413c195f4
T0masS7 <tomassegurado07@icloud.com>
2026-06-16 19:40:22 +0100

    adicionar página de listagem de utilizadores

--- ElderSense/ElderSense/Pages/Utilizadores/Index.cshtml.cs
+++ ElderSense/ElderSense/Pages/Utilizadores/Index.cshtml.cs
@@ -1,12 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
+using ElderSense.Data;
+using ElderSense.Data.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElderSense.Pages.Utilizadores
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        // lista de utilizadores a mostrar na página
+        public IList<Utilizador> Utilizadores { get; set; } = [];
+
+        public async Task OnGetAsync()
+        {
+            // busca todos os utilizadores
+            Utilizadores = await _context.Utilizadores.ToListAsync();
         }
     }
 }
