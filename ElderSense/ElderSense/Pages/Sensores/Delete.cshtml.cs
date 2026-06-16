d4795bd2578dd3ec3d939c9637552e193c2d3fc7
T0masS7 <tomassegurado07@icloud.com>
2026-06-16 16:41:10 +0100

    adicionar página de listagem de sensores

---
+++ ElderSense/ElderSense/Pages/Sensores/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ElderSense.Pages.Sensores
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
