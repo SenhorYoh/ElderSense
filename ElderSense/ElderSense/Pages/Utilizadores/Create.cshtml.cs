a5882000fdb781ea885889c327430bf03bac136c
T0masS7 <tomassegurado07@icloud.com>
2026-06-16 19:37:55 +0100

    criar razor pages utilizador

---
+++ ElderSense/ElderSense/Pages/Utilizadores/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ElderSense.Pages.Utilizadores
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ElderSense/ElderSense/Pages/Utilizadores/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ElderSense.Pages.Utilizadores
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
