6a6756d4e172c90dde54fa5fa69c2ff2dab62637
T0masS7 <tomassegurado07@icloud.com>
2026-06-16 12:54:42 +0100

    corrigir sensor.cs

--- ElderSense/ElderSense/Data/Model/Sensor.cs
+++ ElderSense/ElderSense/Data/Model/Sensor.cs
@@ -1,14 +1,15 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using ElderSense.Data.Model;
 
 namespace ElderSense.Data.Model
 {
     /// <summary>
     /// classe dedicada aos sensores e suas informações
     /// </summary>
-    public class Sensor{
-
+    public class Sensor
+    {
         [Key]
         public int Id { get; set; }
 
@@ -19,12 +20,14 @@ namespace ElderSense.Data.Model
         [Display(Name = "Estado")]
         public bool Estado { get; set; }
 
-
         /// <summary>
-        /// Relacionamento 1-N com a classe Utilizador
+        /// Relacionamento 1-N com a classe Utilizador (Regra 4)
         /// </summary>
         [Display(Name = "Responsável")]
         [ForeignKey(nameof(Utilizador))]
-        public int FKUtilizador { get; set; }
+        public string FKUtilizador { get; set; } = "";
+
+        // navigation property para o Utilizador
+        public Utilizador Utilizador { get; set; } = null!;
     }
 }
