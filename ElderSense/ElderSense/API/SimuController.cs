4901b522847b3452e055c885c7011b175c0803fe
SenhorYoh <yohanenrique8@gmail.com>
2026-06-19 17:11:06 +0100

    Criação da classe para gerar dados falsos para as tabelas Sensores e DadosMonitorizacao

---
+++ ElderSense/ElderSense/API/SimuController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using ElderSense.Data.Model; 
+
+namespace ElderSense.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SimuladorController : ControllerBase
+    {
+        // Se já tiveres o teu ApplicationDbContext ou o contexto da BD criado, 
+        // injeta-o aqui no construtor para poderes gravar os dados falsos.
+
+        public SimuladorController()
+        {
+        }
+
+        [HttpPost("gerar-dados-teste")]
+        public async Task<IActionResult> GerarDadosFalsos()
+        {
+            // O código para gerar os batimentos e alertas vai ficar aqui dentro!
+            return Ok(new { mensagem = "Simulador pronto a funcionar!" });
+        }
+    }
+}
